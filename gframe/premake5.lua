06cb96f80b8e71277ec4ae4f4d044b085112481b
Unicorn369 <3038572935@qq.com>
2024-05-26 09:38:25 +0800

    Merge

--- gframe/premake5.lua
+++ gframe/premake5.lua
@@ -4,9 +4,11 @@ include "spmemvfs/."
 end
 
 project "ygopro"
-if SERVER_MODE then
+if SERVER_MODE_DLL then
+    kind "SharedLib"
+else
     kind "ConsoleApp"
-
+end
     defines { "YGOPRO_SERVER_MODE" }
 
     files { "gframe.cpp", "config.h",
@@ -17,6 +19,9 @@ if SERVER_MODE then
             "netserver.cpp", "netserver.h",
             "single_duel.cpp", "single_duel.h",
             "tag_duel.cpp", "tag_duel.h" }
+    if SERVER_MODE_DLL then
+        files { "serverapi.cpp", "serverapi.h"}
+    end
     includedirs { "../ocgcore" }
     links { "ocgcore", "clzma", LUA_LIB_NAME, "sqlite3", "event" }
     if SERVER_ZIP_SUPPORT then
@@ -28,17 +33,6 @@ if SERVER_MODE then
     end
     if SERVER_PRO2_SUPPORT then
         defines { "SERVER_PRO2_SUPPORT" }
-    end
-else
-    kind "WindowedApp"
-
-    files { "*.cpp", "*.h" }
-    includedirs { "../ocgcore" }
-    links { "ocgcore", "clzma", "cspmemvfs", LUA_LIB_NAME, "sqlite3", "irrlicht", "freetype", "event" }
-end
-
-    if BUILD_IKPMP3 then
-        links { "ikpmp3" }
     end
 
     if BUILD_EVENT then
@@ -79,14 +73,6 @@ end
         libdirs { SQLITE_LIB_DIR }
     end
 
-    if USE_IRRKLANG then
-        defines { "YGOPRO_USE_IRRKLANG" }
-        includedirs { IRRKLANG_INCLUDE_DIR }
-        if not IRRKLANG_PRO then
-            libdirs { IRRKLANG_LIB_DIR }
-        end
-    end
-
     filter "system:windows"
         defines { "_IRR_WCHAR_FILESYSTEM" }
         files "ygopro.rc"
@@ -96,17 +82,6 @@ end
 if SERVER_PRO2_SUPPORT then
         targetname ("AI.Server")
 end
-        if USE_IRRKLANG then
-            links { "irrKlang" }
-            if IRRKLANG_PRO then
-                defines { "IRRKLANG_STATIC" }
-                filter { "not configurations:Debug" }
-                    libdirs { IRRKLANG_PRO_RELEASE_LIB_DIR }
-                filter { "configurations:Debug" }
-                    libdirs { IRRKLANG_PRO_DEBUG_LIB_DIR }
-                filter {}
-            end
-        end
 if SERVER_MODE then
         links { "ws2_32" }
 else
@@ -121,18 +96,7 @@ if not SERVER_MODE then
         links { "z" }
         defines { "GL_SILENCE_DEPRECATION" }
 end
-        if MAC_ARM then
-            buildoptions { "--target=arm64-apple-macos12" }
-            linkoptions { "-arch arm64" }
-        end
-        if USE_IRRKLANG then
-            links { "irrklang" }
-        end
     filter "system:linux"
 if not SERVER_MODE then
         links { "GL", "X11", "Xxf86vm" }
 end
-        if USE_IRRKLANG then
-            links { "IrrKlang" }
-            linkoptions{ IRRKLANG_LINK_RPATH }
-        end
